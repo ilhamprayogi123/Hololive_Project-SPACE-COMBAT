41203af329f691b7c826764dc9312118f8291041
ilhamprayogi123 <ilhamprayogi17@gmail.com>
2023-03-14 19:58:28 +0700

    Enemy Collision
    Make bullet that can destroy enemy

--- Assets/Script/TypeOneEnemy.cs
+++ Assets/Script/TypeOneEnemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TypeOneEnemy : MonoBehaviour
 {
     public float moveSpeed = 5;
+    bool canDestroyed = false;
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,10 @@ public class TypeOneEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < 8f)
+        {
+            canDestroyed = true;
+        }
     }
 
     private void FixedUpdate()
@@ -30,5 +34,20 @@ public class TypeOneEnemy : MonoBehaviour
         }
 
         transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!canDestroyed)
+        {
+            return;
+        }
+
+        BulletManager bullet = collision.GetComponent<BulletManager>();
+        if (bullet != null)
+        {
+            Destroy(gameObject);
+            Destroy(bullet.gameObject);
+        }
     }
 }
